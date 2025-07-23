02299a2ece54b75e52101ddc70f91c0df438a801
Maisa <maisabarbosabispo@gmail.com>
2025-07-22 23:15:06 -0300

    juntando arquivos em ec2 e network

--- infra/network.tf
+++ infra/network.tf
@@ -1,3 +1,12 @@
+resource "aws_vpc" "main_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "mundo-global-vpc"
+  }
+}
+
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.main_vpc.id
   cidr_block              = "10.0.1.0/24"
--- infra/vpc.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_vpc" "main_vpc" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_hostnames = true
-
-  tags = {
-    Name = "mundo-global-vpc"
-  }
-}
